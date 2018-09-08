e2f10f53a6941c1840a095ce67803035f564c0f4
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-09-09 01:12:47 +0800

    updated send email handler

--- python/emailTest.py
+++ python/emailTest.py
@@ -1,9 +1,10 @@
 import emailHandler as eh
-import getpass
 
-login_email = 'gamefakeit6@gmail.com'
-login_password = 'facebookrejects'
-mailserver = eh.login(login_email, login_password)
-# recepient_email = 'contactjrios@gmail.com'
-recepient_email = 'yang025@e.ntu.edu.sg'
-eh.send_mail(mailserver, login_email, recepient_email)
+def send_email():
+    login_email = 'gamefakeit6@gmail.com'
+    login_password = 'facebookrejects'
+    mailserver = eh.login(login_email, login_password)
+    recepient_email = 'contactjrios@gmail.com'
+    # recepient_email = 'yang025@e.ntu.edu.sg'
+    # recepient_email = 'leowyaoyang@gmail.com'
+    eh.send_mail(mailserver, login_email, recepient_email)
