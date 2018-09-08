ff4136a02e5da269b6b266c1e5f4e454faef384b
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-09-09 00:58:41 +0800

    updated email template. updated dashboard

--- python/emailTest.py
+++ python/emailTest.py
@@ -4,5 +4,6 @@ import getpass
 login_email = 'gamefakeit6@gmail.com'
 login_password = 'facebookrejects'
 mailserver = eh.login(login_email, login_password)
-recepient_email = input('Email of receipient: ')
+# recepient_email = 'contactjrios@gmail.com'
+recepient_email = 'yang025@e.ntu.edu.sg'
 eh.send_mail(mailserver, login_email, recepient_email)
