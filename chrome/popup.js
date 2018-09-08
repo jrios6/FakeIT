03c86b46582a9ccc6545943a5e5b7b60c8084795
Ong Jia Rui <ongjiarui@hotmail.com>
2018-09-09 01:26:37 +0800

    Added level up and GM video

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,128 +1,83 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
-
-// let changeColor = document.getElementById('changeColor');
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-//
-// changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
 
 function updateTime(prev) {
   var secondsLeft = 24*60*60 - Math.floor((Date.now()-prev)/1000);
   const hoursLeft =  Math.floor(secondsLeft / 3600);
   var minsLeft = Math.floor((secondsLeft - hoursLeft * 3600) / 60)
-  secondsLeft = (secondsLeft - hoursLeft * 3600 - minsLeft * 60)
+  //secondsDisplay = (secondsLeft - hoursLeft * 3600 - minsLeft * 60)
   if (minsLeft < 10) minsLeft = '0' + minsLeft;
   $('#time').text(`${hoursLeft}:${minsLeft}`);
+
+  if (secondsLeft <= 0) {
+    var notifOptions = {
+      type: 'basic',
+      iconUrl: '/img/magician.png',
+      title: 'Time \'s up!',
+      message: 'Here are the fake news you couldn\'t find',
+    };
+    chrome.notifications.create('timesUpNotif', notifOptions);
+  }
 }
 
+
+function hideStatus() {
+  setTimeout(function() {
+    $('#status').text('');
+  }, 3000);
+}
 
 function updateScores() {
   chrome.storage.sync.get(['score', 'time', 'points'], function(results){
-    $('#score').text(results.score);
+    $('#score').text(`${results.score}/5`);
     $('#points').text(results.points);
     updateTime(results.time);
-
-    if (secondsLeft <= 0) {
-      var notifOptions = {
-        type: 'basic',
-        iconUrl: '/img/viking.png',
-        title: 'Time \'s up!',
-        message: 'Here are the fake news you couldn\'t find',
-      };
-      chrome.notifications.create('timesUpNotif', notifOptions);
-    };
   });
 }
+
+var levelNames = ['Ogre', 'Swordsman', 'Viking', 'Magician', 'Grandmaster'];
 
 
 $(document).ready(function () {
   updateScores();
+
   $('#checkHeadline').click(function(){
     var submission = $('#headline').val();
-    chrome.storage.sync.get("posts", function(response){
-      if (submission == response.posts[0].siteUrl) {
-        chrome.storage.sync.get(['score', 'points'], function(results){
-          var score = results.score + 1;
-          var points = results.points + 50;
-          chrome.storage.sync.set({ score, points });
+    chrome.storage.sync.get(['score', 'points', 'level', 'posts','remainingAttempts'], function(response){
+      if (response.remainingAttempts == 0) {
+        $('#status').text('You ran out of attempts! Here are the fake articles.');
+        hideStatus();
+      } else if (submission == response.posts[0].siteUrl) {
+        if (response.posts[0].done) {
+          $('#status').text('You have already entered this!');
+          hideStatus();
+        } else {
+          var score = response.score + 1;
+          var points = response.points + 50;
+          var level = response.level
+
+          if (score == 5) {
+            level += 1
+            $('#status').text(`Congrats for getting all 5 right! You are now a ${levelNames[level]}🎉`);
+            $('#video').html('<video controls autoplay width-"400px"><source src="gm.mp4" type="video/mp4"></video>');
+          } else {
+            $('#status').text('Correct! You earned 50 points :)');
+          }
+          response.posts[0].done = true;
+          chrome.storage.sync.set({ score, points, posts: response.posts, level });
           updateScores();
-        });
-        $('#status').text('Correct! You earned 50 points :)');
-        setTimeout(function() {
-          $('#status').text('');
-        }, 2500);
+        }
       } else {
-        $('#status').text('Wrong! You got x remaining attempts today.');
-        setTimeout(function() {
-          $('#status').text('');
-        }, 2500);
+        var remainingAttempts = response.remainingAttempts - 1;
+        chrome.storage.sync.set({ remainingAttempts });
+        $('#status').text(`Wrong! You got ${remainingAttempts} remaining attempts today.`);
       }
     });
   });
 });
 
 
-
-  //
-  // $('#spendAmount').click(functiom(){
-  //   chrome.store.sync.get('total', function(budget){
-  //     var newTotal = 0;
-  //     if (budget.total) {
-  //       newTotal += parseInt(budget.total);
-  //     }
-  //
-  //     var amount = $('#amount').val();
-  //     if(amount) {
-  //       newTotal += parseInt(amount);
-  //
-  //       chrome.storage.sync.set({'total': newTotal}, function(){
-  //         if (amount) {
-  //           var notifOptions = {
-  //             type: 'basic',
-  //             iconUrl: 'icon48.png',
-  //             title: 'Limit reached!',
-  //             message: 'limit reached!'
-  //           };
-  //           chrome.notifications.create('limitNotif', notifOptions);
-  //         }
-  //       });
-  //       $('#total').text(newTotal);
-  //       $('#amount').val('');
-
-
-// CODE for using jquery to retrieve input value, update chrome stoage and update text fields
-// $(function(){
-//   chrome.storage.sync.get(['total', 'limit'], function(budget){
-//     $('#total').text(budget.total);
-
-//   });
-//
-
-
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-
-  }
-});
-
-
 function onWindowLoad() {
   var message = document.querySelector('#message');
-
-
   chrome.tabs.executeScript(null, {
     file: "getPagesSource.js"
   }, function() {
