9119e5ee0f9fc12cfa869c3332bd2d3a81aa0d7c
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-09-08 18:52:33 +0800

    Styled RSS Feed. Added week by week graph

--- dashboard/js/graph.js
+++ dashboard/js/graph.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var ctx = document.getElementById("barChart").getContext('2d');
+    var ctx = document.getElementById("weekChart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -22,6 +22,53 @@ window.onload = function() {
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(6, 131, 151, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    var monthctx = document.getElementById("monthChart").getContext('2d');
+    var myChart2 = new Chart(monthctx, {
+        type: 'line',
+        data: {
+            labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6", "Week 7"],
+            datasets: [{
+                label: '# correctly answered',
+                data: [3, 1, 2, 3, 4, 5, 5],
+                backgroundColor: [
+
+                    'rgba(54, 162, 235, 0.2)',
+
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(6, 131, 151, 0.2)'
+                ],
+                borderColor: [
+
+                    'rgba(54, 162, 235, 1)',
+
+                    'rgba(255,99,132,1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 206, 86, 1)',
                     'rgba(255, 159, 64, 1)',
                     'rgba(6, 131, 151, 1)'
                 ],
