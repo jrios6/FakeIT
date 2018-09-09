9e7491c964061ef9798b45c6e20e96710b721d14
Ong Jia Rui <ongjiarui@hotmail.com>
2018-09-09 09:20:12 +0800

    Updatez

--- python/emailTest.py
+++ python/emailTest.py
@@ -4,7 +4,7 @@ def send_email():
     login_email = 'gamefakeit6@gmail.com'
     login_password = 'facebookrejects'
     mailserver = eh.login(login_email, login_password)
-    # recepient_email = 'contactjrios@gmail.com'
-    recepient_email = 'yang025@e.ntu.edu.sg'
+    recepient_email = 'contactjrios@gmail.com'
+    # recepient_email = 'yang025@e.ntu.edu.sg'
     # recepient_email = 'leowyaoyang@gmail.com'
     eh.send_mail(mailserver, login_email, recepient_email)
